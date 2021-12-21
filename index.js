e023188e8693e9d519cffbbc41446ae8241e2b4e
unknown <nad279444>
2021-12-21 08:15:37 -0800

    dynammically rendered port

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ mongoose.connect(db,
 {useNewUrlParser:true,useUnifiedTopology:true})
 .then(() => {console.log('database is connected live')})
 
-const port = 3000;
+const port = process.env.P0RT || 3000;
 
 
 app.use(cors());
 app.use(express.json());
 app.use('/todos', router);
 
-// app.get('/',(req,res) => {
-//      res.send('Welcome back Express');
-//  })
+app.get('/todos',(req,res) => {
+     res.send('Welcome back Express');
+ })
     
 
 app.listen(port, () => {
