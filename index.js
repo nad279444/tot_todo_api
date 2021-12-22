e705db73c8bd014c1211a7249ccdff051cfc6481
unknown <nad279444>
2021-12-22 02:07:57 -0800

    added a catch method

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = process.env.DB_URL;
 
 mongoose.connect(db,
 {useNewUrlParser:true,useUnifiedTopology:true})
-.then(() => {console.log('database is connected live')})
+.then(() => {console.log('database is connected live')}).catch((error) => `unable to connect ${error}` )
 
 const port = process.env.P0RT || 3000;
 
