f89bffb71fe8d6e22fc94026059c26a44caec09e
unknown <nad279444>
2022-01-08 04:27:17 -0800

    test

--- api/routes/todoRoute.js
+++ api/routes/todoRoute.js
@@ -6,7 +6,7 @@ import Todo from '../model/todoModel.js';
 const router = express.Router();
 const app = express();
 
-app.get('/',(req,res) => {
+router.get('/',(req,res) => {
   res.send('Welcome back Express');
 })
 
